a2d98eea443674ab98c660102137d2c5ffb24b2b
palkopatel@gmail.com <palkopatel@gmail.com@1819cdf6-058e-6671-93ae-ff36ddda1318>
2010-10-17 16:53:41 +0000

    * не всегда есть серия в заголовке * ошибка: join -> left join

--- pryced.py
+++ pryced.py
@@ -34,23 +34,36 @@ def load_link(url_name, create_flag):
    """ загрузка новой книги в свою базу
    
    """
-   f = urllib2.urlopen(url_name) 
-   datas = f.read()
-   f.close()
-   soup = BeautifulSoup(datas)
-   if create_flag > 0:
-      (connect, cursor) = connect_to_base()
-      try:
-         fields = soup.find('title').string.split(' | ')
-         cursor.execute( 'insert into links (urlname, title, author, serial, desc1, desc2) values (?, ?, ?, ?, ?, ?)', 
-                         (url_name, fields[1], fields[2], fields[3], fields[4], fields[0]) )
-         cursor.close()
-         connect.commit()
-         connect.close()
-         print "commit link ok."
-      except sqlite3.Error, e:
-         print "An error occurred:", e.args[0]
-   return soup.find('big').string
+   try:
+      f = urllib2.urlopen(url_name) 
+      datas = f.read()
+      f.close()
+      soup = BeautifulSoup(datas)
+      if create_flag > 0:
+         (connect, cursor) = connect_to_base()
+         try:
+            fields = soup.find('title').string.split(' | ')
+            desc2 = fields[0]
+            title = fields[1]
+            author = fields[2]
+            try:
+               serial = fields[3] 
+               desc1 = fields[4]
+            except:
+               serial = ''
+               desc1 = fields[3]
+            cursor.execute( 'insert into links (urlname, title, author, serial, desc1, desc2) values (?, ?, ?, ?, ?, ?)', 
+                            (url_name, title, author, serial, desc1, desc2) )
+            cursor.close()
+            connect.commit()
+            connect.close()
+            print "commit link ok."
+         except sqlite3.Error, e:
+            print "An error occurred:", e.args[0]
+      return soup.find('big').string
+   except Exception, e:
+      print e
+      return ''
 
 def load_new_price(now_day):
    """ получение и сохранение в свою базу текущих цен для имеющихся книг
@@ -61,14 +74,14 @@ def load_new_price(now_day):
    cursor.execute('select links.id, links.urlname, links.author || ", " || links.title \
                         , min(prices.price)\
                    from links \
-                   join prices on links.id = prices.link \
+                   left join prices on links.id = prices.link \
                    group by links.id, links.author, links.title')
    rows = cursor.fetchall()
    results = []
    for row in rows:
          # получение текущей цены с сайта
       price = load_link(row[1], 0)
-      print row[1] + ' : ' + row[2] + U'  : сейчас: ' + str(price) + U' : минимум: ' + str(row[3])
+      print row[1] + U' : ' + row[2] + U'  : сейчас: ' + str(price) + U' : минимум: ' + str(row[3])
       results.insert(0, (now_day, row[0], price) )
    cursor.close()
 
@@ -93,7 +106,7 @@ try:
    now_day = datetime.datetime.now().strftime('%Y-%m-%d %H:%M:%S')
 
    if sys.argv[1] == '-a': # добавление ссылки на книгу в базу
-      print( sys.argv[2], ' ', load_link(sys.argv[2], 1) )
+      print( sys.argv[2], U' : цена сейчас: ', load_link(sys.argv[2], 1) )
    elif sys.argv[1] == '-g': # добавление текущих цен в базу
       try:
          load_new_price(now_day)
